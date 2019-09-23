f29732a5da96b4bcf47406b706cdb642d5edc9eb
Kylan O'Connor <kylan02@gmail.com>
2019-09-23 09:33:29 -0700

    make accordion

--- Script.js
+++ Script.js
@@ -10,23 +10,17 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-<<<<<<< HEAD
 
-=======
->>>>>>> 01f8cf5c7382a18efa6fef51922114954690ff9d
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
-<<<<<<< HEAD
    //  if (panel.style.display === "block") {
    //   panel.style.display = "none";
    // } else {
    //   panel.style.display = "block";
    // }
-=======
->>>>>>> 01f8cf5c7382a18efa6fef51922114954690ff9d
   });
 }
 });
