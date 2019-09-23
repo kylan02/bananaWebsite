07a01862ae4891facabe984c67f8705fc0329486
Luca <lucaccorey@gmail.com>
2019-09-23 09:36:19 -0700

    H3 fix for css

--- Script.js
+++ Script.js
@@ -16,11 +16,6 @@ for (i = 0; i < acc.length; i++) {
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
-   //  if (panel.style.display === "block") {
-   //   panel.style.display = "none";
-   // } else {
-   //   panel.style.display = "block";
-   // }
   });
 }
 $("#a").hover(function(){
