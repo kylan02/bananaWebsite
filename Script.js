397a39f98d8c59ba7094f111c8565281ea4580bf
Luca <lucaccorey@gmail.com>
2019-09-23 09:27:25 -0700

    Jquery Gallery and fix css h3

--- Script.js
+++ Script.js
@@ -10,11 +10,14 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-     panel.style.display = "none";
-   } else {
-     panel.style.display = "block";
-   }
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   });
 }
 });
+$("#a").hover(function(){
+    $("#one").slideToggle("slow");
+  });
