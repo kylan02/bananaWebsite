c01d86c814b2581d6412913b0feabb462ae018dd
LEGOSROCKDUDE86 <arriagabrandon231@gmail.com>
2019-09-23 17:25:20 -0700

    changed carousel, added small drop shadow, fixwed history button

--- Script.js
+++ Script.js
@@ -1,30 +1,39 @@
-$(document).ready(function(){
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(document).ready(function() {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
 
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-$("#a").hover(function(){
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+  $("#a").hover(function() {
     $("#one").slideToggle("slow");
   });
-  $("#b").hover(function(){
-      $("#two").slideToggle("slow");
-    });
-    $("#c").hover(function(){
-        $("#three").slideToggle("slow");
-      });
+  $("#b").hover(function() {
+    $("#two").slideToggle("slow");
+  });
+  $("#c").hover(function() {
+    $("#three").slideToggle("slow");
+  });
+  // history of the banana button
+  $(".history").hide();
+  $(".button").click(function() {
+    $(".history").toggle();
+  });
+  // carousel duration
+  $("#carouselExampleSlidesOnly").carousel({
+    interval: 500;
+  });
 });
